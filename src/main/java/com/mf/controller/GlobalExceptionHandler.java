20cbbdeefd20153314674599fb7e70fcf86d7cad
2810164528@qq.com <2810164528@qq.com>
2021-02-07 09:53:42 +0800

    fix:删除GlobalExceptionHandler类中无用注解

--- src/main/java/com/mf/controller/GlobalExceptionHandler.java
+++ src/main/java/com/mf/controller/GlobalExceptionHandler.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.constraints.NotNull;
-
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @Autowired
@@ -17,7 +14,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public CommonResponse handleException(@NotNull Exception e) {
+    public CommonResponse handleException(Exception e) {
         String msg = e.getMessage() + (e.getCause() == null ? "" : "[" + e.getCause().getMessage() + "]");
         logService.printLog("出现异常：" + msg);
         return new CommonResponse("9999", msg, null);
